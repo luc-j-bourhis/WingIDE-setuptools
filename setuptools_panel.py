5d82ef8e3adf6b1e64c11cd426629ee1bc88271b
Luc J. Bourhis <luc_j_bourhis@mac.com>
2015-03-14 03:09:44 +0100

    Use Qt4 signal properties everywhere
    Method and variable names changed to comply with PEP 8.
    Fix outdated code documentation.

--- setuptools_panel.py
+++ setuptools_panel.py
@@ -57,12 +57,12 @@ _i18n_module = 'scripts_setuptools_panel'
 def setuptools_build_in_place():
     """ Build in-place using Setuptools """
     view = wingapi.gApplication.fSingletons.fGuiMgr.ShowPanel(_kPanelID)
-    view._Build()
+    view.build()
 
 def setuptools_clean_all():
     """ Clean all files produced by `setuptools_build_in_place` """
     view = wingapi.gApplication.fSingletons.fGuiMgr.ShowPanel(_kPanelID)
-    view._Clean()
+    view.clean()
 
 # End of commands
 
@@ -85,16 +85,27 @@ class _CSetuptoolsPanelDefn(dockview.CPanelDefn):
                              size=(350, 1000))
 
     def _CreateView(self):
+        """Create a new view for this panel type
+
+           Implement parent abstract method.
+        """
         return _CSetuptoolsView(self.fSingletons)
 
     def _GetLabel(self, panel_instance):
-        """Get (label, prefix, suffix) for the display label to use for the
-        given panel instance"""
+        """ Get (label, prefix, suffix) for the display label to use
+            for the given panel instance
+
+            Implement parent abstract method.
+        """
         return _('Setuptools'), '', ''
 
     def _GetTitle(self, panel_instance):
-        """Get full title for the given panel instance"""
+        """ Get full title for the given panel instance
+
+            Implement parent abstract method.
+        """
         return _('Setuptools Panel')
+
 
 class _CSetuptoolsViewCommands(commandmgr.CClassCommandMap):
 
@@ -127,10 +138,6 @@ class _CSetuptoolsView(wingview.CViewController):
 
         There is also a Log tab which displays the full text output by
         the build command.
-
-        TODO: at the moment, only detect Cython errors; C/C++ compiler error
-        patterns are different for different compilers but one can guess
-        the compiler in use by looking at the output of the command above.
     """
 
     def __init__(self, singletons):
@@ -147,7 +154,7 @@ class _CSetuptoolsView(wingview.CViewController):
         self.__CreateGui()
 
     def _destroy_impl(self):
-        wgtk.Destroy(self.fErrorList)
+        wgtk.Destroy(self._error_list)
 
     ##########################################################################
     # Inherited calls from wingview.CViewController
@@ -169,60 +176,56 @@ class _CSetuptoolsView(wingview.CViewController):
     ##########################################################################
 
     def __CreateGui(self):
-        self.fNotebook = wgtk.Notebook()
+        self._notebook = wgtk.Notebook()
 
-        self.fBuildButton = wgtk.IconButton(
+        self._build_button = wgtk.IconButton(
             icon=wgtk.STOCK_EXECUTE, relief=wgtk.RELIEF_NONE,
             border_width=0, focus_on_click=False)
-        wgtk.gui_connect(self.fBuildButton, 'clicked',
-                         wgtk.NoObjCallback(self._Build))
-        self.fBuildButton.set_tip(_('Build in-place'))
+        self._build_button.clicked.connect(self.build)
+        self._build_button.set_tip(_('Build in-place'))
 
-        self.fCleanButton = wgtk.IconButton(
+        self._clean_button = wgtk.IconButton(
             icon='wingide-trash', relief=wgtk.RELIEF_NONE,
             border_width=0, focus_on_click=False)
-        wgtk.gui_connect(self.fCleanButton, 'clicked',
-                         wgtk.NoObjCallback(self._Clean))
-        self.fCleanButton.set_tip(_('Clean'))
+        self._clean_button.clicked.connect(self.clean)
+        self._clean_button.set_tip(_('Clean'))
 
-        self.fTerminateButton = wgtk.IconButton(
+        self._terminate_button = wgtk.IconButton(
             icon='wingide-os-commands-stop', relief=wgtk.RELIEF_NONE,
             border_width=0, focus_on_click=False)
-        wgtk.gui_connect(self.fTerminateButton, 'clicked',
-                         wgtk.NoObjCallback(self._Terminate))
-        self.fTerminateButton.set_tip(_('Terminate build'))
-        self.fTerminateButton.setEnabled(False)
+        self._terminate_button.clicked.connect(self.terminate)
+        self._terminate_button.set_tip(_('Terminate build'))
+        self._terminate_button.setEnabled(False)
 
         top_hbox = wgtk.HBox(visible=True)
-        top_hbox.pack_start(self.fBuildButton, expand=False)
-        top_hbox.pack_start(self.fCleanButton, expand=False)
-        top_hbox.pack_start(self.fTerminateButton, expand=False)
+        top_hbox.pack_start(self._build_button, expand=False)
+        top_hbox.pack_start(self._clean_button, expand=False)
+        top_hbox.pack_start(self._terminate_button, expand=False)
 
-        tree = wgtk.SimpleList(
+        self._error_list = wgtk.SimpleList(
             [_("File"), _("Line"), _("Column"), _("Message")],
             plain_text=True)
         for i in (0, 1):
-            tree.hideColumn(i)
-        tree.clicked.connect(self._OnClickedErrorItem)
-        wgtk.InitialShow(tree)
-        self.fErrorList = tree
+            self._error_list.hideColumn(i)
+        self._error_list.clicked.connect(self._on_click_error_item)
+        wgtk.InitialShow(self._error_list)
 
-        self.fErrorTabLabel = wgtk.QLabel('Errors/Warnings')
-        self.fErrorTabLabel.setToolTip('Build errors and warnings')
-        self.fNotebook.append_page(self.fErrorList, self.fErrorTabLabel)
+        self._error_tab_label = wgtk.QLabel('Errors/Warnings')
+        self._error_tab_label.setToolTip('Build errors and warnings')
+        self._notebook.append_page(self._error_list, self._error_tab_label)
 
-        self.fLog = cap_oscommands.CConsoleView(self.fSingletons)
-        self.fLogTabLabel = wgtk.QLabel('   Log   ')
-        self.fLog._fScint.set_wrap_mode(True)
-        self.fLogTabLabel.setToolTip('Log of the last build')
-        self.fNotebook.append_page(self.fLog.fGtkWidget, self.fLogTabLabel)
+        self._log = cap_oscommands.CConsoleView(self.fSingletons)
+        self._log_tab_label = wgtk.QLabel('   Log   ')
+        self._log._fScint.set_wrap_mode(True)
+        self._log_tab_label.setToolTip('Log of the last build')
+        self._notebook.append_page(self._log.fGtkWidget, self._log_tab_label)
 
-        self.fStatus = wgtk.Label()
+        self._status = wgtk.Label()
 
         vbox = wgtk.VBox(visible=True)
         vbox.pack_start(top_hbox, expand=False)
-        vbox.pack_start(self.fStatus, expand=False)
-        vbox.pack_start(self.fNotebook, expand=True)
+        vbox.pack_start(self._status, expand=False)
+        vbox.pack_start(self._notebook, expand=True)
 
         self._SetGtkWidget(vbox)
 
@@ -231,7 +234,7 @@ class _CSetuptoolsView(wingview.CViewController):
                 line \s+ (?P<line>\d+) .*?
              ^ (?P<message> \w+ Error: .+?) $
         ''', flags=re.X|re.M|re.S)
-    cython_error_pattern = re.compile(
+    cython_clang_gcc_error_pattern = re.compile(
         r''' ^ (?P<filename>[^:\n]+?) : (?P<line>\d+) : (?P<column>\d+)
                 : [ ] (?P<message>.+?) $
         ''', flags=re.X|re.M|re.S)
@@ -247,7 +250,7 @@ class _CSetuptoolsView(wingview.CViewController):
     setuptools_build_in_place_action = '{}()'.format(
         setuptools_build_in_place.__name__)
 
-    def _ProjectDir(self):
+    def project_dir(self):
         my_proj = wingapi.gApplication.GetProject()
         if my_proj is None:
             wingapi.gApplication.ShowMessageDialog(
@@ -257,17 +260,17 @@ class _CSetuptoolsView(wingview.CViewController):
             return None
         return os.path.dirname(my_proj.GetFilename())
 
-    def _Execute(self, setup_py_args, postprocess=None):
+    def execute(self, setup_py_args, postprocess=None):
         """ Execute setup.py with the given command line arguments """
         import proj.project
 
-        projectDir = self._ProjectDir()
+        projectDir = self.project_dir()
         if projectDir is None:
             return
         my_proj = wingapi.gApplication.GetProject()
 
-        setupDotPy = os.path.join(projectDir, 'setup.py')
-        if not os.path.isfile(setupDotPy):
+        setup_dot_py = os.path.join(projectDir, 'setup.py')
+        if not os.path.isfile(setup_dot_py):
             wingapi.gApplication.ShowMessageDialog(
                 title='Setuptools Build',
                 text='You need to create a file setup.py in your project '
@@ -275,9 +278,9 @@ class _CSetuptoolsView(wingview.CViewController):
                 sheet=True)
             return
 
-        saveMgr = self.fSingletons.fGuiMgr.fSaveMgr
+        save_mgr = self.fSingletons.fGuiMgr.fSaveMgr
         savable_list = []
-        for savable in saveMgr.GetItemsToSave():
+        for savable in save_mgr.GetItemsToSave():
             if isinstance(savable.fLocation, location.CUnknownLocation):
                 continue # unsaved
             if isinstance(savable, proj.project.CProject):
@@ -285,34 +288,34 @@ class _CSetuptoolsView(wingview.CViewController):
             if savable._fAutoSave is not None and not savable._fAutoSave:
                 continue # looks like this one is marked for not auto-saving
             savable_list.append(savable)
-        saveMgr.PromptForSave(
+        save_mgr.PromptForSave(
             savable_list, initial_prompt=False, can_cancel=False)
 
         encoding = encoding_utils.kDefaultConsoleOutputEncoding
 
         # buffer_size = 1 for for line mode
-        cmd = (my_proj.GetPythonExecutable(setupDotPy), "-u", "setup.py")
+        cmd = (my_proj.GetPythonExecutable(setup_dot_py), "-u", "setup.py")
         self.output = ""
-        self.fLog._Clear()
+        self._log._Clear()
         self.child_process = spawn.CChildProcess(
             cmd + setup_py_args,
-            env=my_proj.GetEnvironment(setupDotPy),
+            env=my_proj.GetEnvironment(setup_dot_py),
             child_pwd=projectDir,
             io_encoding=encoding, buffer_size=1)
-        self.fBuildButton.setEnabled(False)
-        self.fCleanButton.setEnabled(False)
-        self.fTerminateButton.setEnabled(True)
-        self.fStatus.set_text("Running...")
+        self._build_button.setEnabled(False)
+        self._clean_button.setEnabled(False)
+        self._terminate_button.setEnabled(True)
+        self._status.set_text("Running...")
 
         def received_output(child, text):
             self.output += text
-            self.fLog.AppendOutput(text)
-            self.fLogTabLabel.setStyleSheet("QLabel { color : red; }")
+            self._log.AppendOutput(text)
+            self._log_tab_label.setStyleSheet("QLabel { color : red; }")
 
         def terminated(child_process):
             if postprocess is not None:
                 postprocess()
-            self.fLogTabLabel.setStyleSheet("QLabel { color : black; }")
+            self._log_tab_label.setStyleSheet("QLabel { color : black; }")
             contents = []
             using_msvc = self.output.find('Microsoft Visual Studio') >= 0
             for m in self.python_error_pattern.finditer(self.output):
@@ -323,18 +326,18 @@ class _CSetuptoolsView(wingview.CViewController):
                     contents.append(
                         m.group('filename', 'line') + ('', m.group('message')))
             else:
-                for m in self.cython_error_pattern.finditer(self.output):
+                for m in self.cython_clang_gcc_error_pattern.finditer(self.output):
                     contents.append(
                         m.group('filename', 'line', 'column', 'message'))
-            self.fErrorList.set_contents(contents)
-            self.fBuildButton.setEnabled(True)
-            self.fCleanButton.setEnabled(True)
-            self.fTerminateButton.setEnabled(False)
-            self.fStatus.set_text('')
+            self._error_list.set_contents(contents)
+            self._build_button.setEnabled(True)
+            self._clean_button.setEnabled(True)
+            self._terminate_button.setEnabled(False)
+            self._status.set_text('')
 
         def start_failed(child_process, exc):
-            self.fTerminateButton.setEnabled(False)
-            self.fStatus.set_text('')
+            self._terminate_button.setEnabled(False)
+            self._status.set_text('')
             wingapi.gApplication.ShowMessageDialog(
                 title='Setuptools Build',
                 text='Internal error: {}'.format(exc),
@@ -352,23 +355,23 @@ class _CSetuptoolsView(wingview.CViewController):
             self.child_process.destroy()
             self.child_process = None
 
-    def _Build(self):
+    def build(self):
         """ Build in-place """
-        self.fErrorList.set_contents([])
-        self._Execute(setup_py_args=("build_ext", "-i"))
+        self._error_list.set_contents([])
+        self.execute(setup_py_args=("build_ext", "-i"))
 
-    def _Clean(self):
+    def clean(self):
         """ Remove all files produced by builds so far
 
             The command clean does remove files from the source directory:
             this is a known limitation of Setuptools. Thus we roll our own
             hand-made heuristic.
         """
-        self._Execute(setup_py_args=("clean", "-a"),
+        self.execute(setup_py_args=("clean", "-a"),
                       postprocess=self.clean_source_directory)
 
     def clean_source_directory(self):
-        projectDir = self._ProjectDir()
+        projectDir = self.project_dir()
         if projectDir is None:
             return
         cleanees = []
@@ -385,21 +388,21 @@ class _CSetuptoolsView(wingview.CViewController):
                                          fo.readline(), re.I|re.X):
                                 cleanees.append(full_path)
         for f in cleanees:
-            self.fLog.AppendOutput(
+            self._log.AppendOutput(
                 "Removing %s\n" % os.path.relpath(f, projectDir))
             os.unlink(f)
 
-    def _Terminate(self):
+    def terminate(self):
         """ Called when the terminate button is clicked """
         self.child_process.Kill()
-        self.fTerminateButton.setEnabled(False)
-        self.fStatus.set_text('')
+        self._terminate_button.setEnabled(False)
+        self._status.set_text('')
 
-    def _OnClickedErrorItem(self, index):
+    def _on_click_error_item(self, index):
         """ Called when the user clicks a line in the error list """
-        selected = self.fErrorList.GetSelectedContent()
+        selected = self._error_list.GetSelectedContent()
         if selected is not None and len(selected) != 0:
-            filename = os.path.join(self._ProjectDir(), selected[0][0])
+            filename = os.path.join(self.project_dir(), selected[0][0])
             line = int(selected[0][1])
             col_txt = selected[0][2]
             col = int(col_txt) if col_txt else 0
